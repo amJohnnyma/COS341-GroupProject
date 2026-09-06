df35a031a579d76ea441390a424d1f457ebb3275
HeinrichWRomer <heinrichwillemromer101@gmail.com>
2026-09-06 13:06:33 +0200

    added string check

--- lexer.py
+++ lexer.py
@@ -7,13 +7,13 @@ class Lexer:
         self.pos = 0
     
     def get_next_token(self):
-        while (self.pos < len(self.source) and self.source[self.pos] in [" ", "\n"]):
+        while (self.pos < len(self.source) and self.source[self.pos] in [" ", "\n", "\r"]):
             self.pos += 1
         if self.pos >= len(self.source):
             return None
         startPos = self.pos
 
-        while (self.pos < len(self.source) and self.source[self.pos] not in [" ", "\n"]):
+        while (self.pos < len(self.source) and self.source[self.pos] not in [" ", "\n", "\r"]):
             self.pos += 1
             
         token = self.source[startPos:self.pos]
@@ -31,26 +31,35 @@ class Lexer:
             return self.check_keyword_symbol(token)
     
     def check_UDN(self, token):
-        pass
+        for char in token[1:]:
+            if not (('a' <= char <= 'z') or ('0' <= char <= '9')):
+                raise ValueError(f"UDN: {token} is invalid")
+        return (token)
     
     def check_string(self, token):
-        pass
+        if not token.endswith('"'):
+            raise ValueError(f"String: {token} is invalid")
+        for char in token[1:-1]:
+            if not (('a' <= char <= 'z') or ('0' <= char <= '9') or (char == ',') or (char == '.') or (char == ':') or (char == '-') or (char == '?') or (char == '!')):
+                raise ValueError(f"String: {token} is invalid")
+
+        return (token)
     
     def check_num(self, token):
         pass
     
     def check_keyword_symbol(self, token):
-        pass
+        return "testing"
 
     def look_ahead(self):
         temp_pos = self.pos
-        while (temp_pos < len(self.source) and self.source[temp_pos] in [" ", "\n"]):
+        while (temp_pos < len(self.source) and self.source[temp_pos] in [" ", "\n", "\r"]):
             temp_pos += 1
         if temp_pos >= len(self.source):
             return None
         startPos = temp_pos
 
-        while (temp_pos < len(self.source) and self.source[temp_pos] not in [" ", "\n"]):
+        while (temp_pos < len(self.source) and self.source[temp_pos] not in [" ", "\n", "\r"]):
             temp_pos += 1
             
         token = self.source[startPos:temp_pos]
